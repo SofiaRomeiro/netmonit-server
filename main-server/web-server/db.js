88cb87cd0c897c45122b505d44d97cc0f2094c1f
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2023-03-26 20:06:47 +0100

    update db.js

--- main-server/web-server/db.js
+++ main-server/web-server/db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: DBUSER,
-    password: DBPASSWORD,
-    host: DOCKER_IP, // docker container IP address
-    port: DBPORT,
-    database: DBNAME
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    host: process.env.DOCKER_IP, // docker container IP address
+    port: process.env.DBPORT,
+    database: process.env.DBNAME
 });
 
 module.exports = pool;
